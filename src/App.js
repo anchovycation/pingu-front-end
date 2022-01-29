92b9525c454c8e79dd3da012f6cf9760a9823e34
beyzaerkan <beyzaerkan@icloud.com>
2022-01-29 23:24:55 +0300

    created landing page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
-import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
-      <Link to='/about'>about</Link>
-    </div>
+    <div className='App'>
+    <div className='container'>
+      <div className="row py-5">
+        <div className="col-md-6 text">
+          <h1>The best way to <span className='penguening'>penguening</span> with friends.</h1>
+          <p className='info'>start watching a YouTube video now</p>
+          <a href="/create-room" className='createRoomBtn'>Create a Room</a>
+          <button type="button" class="btn btn-link joinBtn">Join a Room</button>  
+        </div>
+        <div className="col-md-6">
+          <img src="/pingu.png" alt="pingu" className='img-fluid' width="500px" height="500px"/>
+        </div>
+      </div>
+    </div> 
+  </div>
   );
 }
 
