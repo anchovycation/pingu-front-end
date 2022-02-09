9cf91e04f36fccef9fb84101e791be718c747a94
beyzaerkan <beyzaerkan@icloud.com>
2022-02-09 19:02:59 +0300

    grid system created for watching room

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import App from './App';
 import CreateRoomPage from './Pages/CreateRoom/CreateRoom';
 import JoinRoomPage from './Pages/JoinRoom/JoinRoom';
 import JoinRoomCheckerPage from './Pages/JoinRoomChecker/JoinRoomChecker';
+import WatchingRoomPage from './Pages/WatchingRoom/WatchingRoom';
 
 import './styles/index.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -22,6 +23,7 @@ ReactDOM.render(
         <Route path="/create-room" element={<CreateRoomPage />} />
         <Route path="/join-room" element={<JoinRoomPage />} />
         <Route path="/join-room/:roomId" element={<JoinRoomCheckerPage />} />
+        <Route path="/rooms/:roomId" element={<WatchingRoomPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
