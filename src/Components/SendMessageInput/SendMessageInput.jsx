843b22842c0fcb4eb31e9e4833123b92766e5ee0
beyzaerkan <beyzaerkan@icloud.com>
2022-07-09 12:54:11 +0300

    context created

--- src/Components/SendMessageInput/SendMessageInput.jsx
+++ src/Components/SendMessageInput/SendMessageInput.jsx
@@ -1,25 +1,26 @@
-import { React, useState } from "react";
+import { React, useState, useContext } from "react";
 import TextInput from "../TextInput/TextInput";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
+import { Context } from "../../Contexts/SendMessageInputContext";
 
 import './SendMessageInput.scss'
 
-function SendMessageInput (props) {
-  const [message, setMessage] = useState('');
+function SendMessageInput () {
+  const { setText, click } = useContext(Context);
 
   return (
     <div className="send-input">
       <div className="row">
         <div className="col-10">
           <TextInput
-            valueSetter={setMessage}
+            valueSetter={setText}
             placeholder='Hey! How you doin?'
             isFocused={true}
           />
         </div>
         <div className="col-2 icon">
-          <FontAwesomeIcon icon={faPaperPlane}/>
+          <FontAwesomeIcon icon={faPaperPlane} onClick={click}/>
         </div>
       </div>      
     </div>
