5827593cc1c8f5002e226a1b747299389a7e4692
Beyza Erkan <76122007+beyzaerkan@users.noreply.github.com>
2022-09-16 11:16:56 +0300

    change-video-duration event added (#66)

--- src/Components/YouTubePlayer/YouTubePlayer.jsx
+++ src/Components/YouTubePlayer/YouTubePlayer.jsx
@@ -13,6 +13,9 @@ const YouTubePlayer = forwardRef((props, ref) => {
   // https://github.com/tjallingt/react-youtube
   const [id, setId] = useState(getIdFromUrl(props.url));
   const [player, setPlayer] = useState({});
+  const [sequence, setSequence] = useState([]);
+  const [timer, setTimer] = useState(null);
+  const [currentTime, setCurrentTime] = useState(0);
 
   useEffect(( )=> {
     setId(getIdFromUrl(props.url));
@@ -33,14 +36,6 @@ const YouTubePlayer = forwardRef((props, ref) => {
     setPlayer(event.target);
   }
 
-  const playVideo = () => {
-    props.setVideoStatus(VIDEO_STATUS.PLAYED);
-  }
-
-  const stopVideo = () => {
-    props.setVideoStatus(VIDEO_STATUS.STOPPED);
-  }
-
 
   useImperativeHandle(ref, () => ({ 
     playVideo: () => {
@@ -48,12 +43,53 @@ const YouTubePlayer = forwardRef((props, ref) => {
     },
     stopVideo: () => {
       player.pauseVideo();
+    },
+    jumpInVideo: (duration) => {
+      player.seekTo(duration);
     } 
   }));
 
+const handleStateChange = event => handleEvent(event.data);
+
+const isSubArrayEnd = (A, B) => {
+  if (A.length < B.length)
+    return false;
+  let i = 0;
+  while (i < B.length) {
+    if (A[A.length - i - 1] !== B[B.length - i - 1]) 
+      return false;
+    i++;
+  }
+  return true;
+};
+
+const handleEvent = type => {
+  setSequence([...sequence, type]);
+  setCurrentTime(player.getCurrentTime());
+  if (type === 1 && isSubArrayEnd(sequence, [3]) && !sequence.includes(-1)) {
+    props.setVideoDuration(currentTime);
+    setSequence([]);
+    return;
+  } else {
+    clearTimeout(timer);
+    if (type !== 3) {
+      let timeout = setTimeout(function () {
+        if (type === 1) {
+          props.setVideoStatus(VIDEO_STATUS.PLAYED);
+        }
+        else if (type === 2) {
+          props.setVideoStatus(VIDEO_STATUS.STOPPED);
+        }
+        setSequence([]);
+      }, 250);
+      setTimer(timeout);
+    }
+  }
+};
+
   return (
     <div className='youtube-player ratio ratio-16x9'>
-      <YouTube videoId={id} opts={opts} onReady={onPlayerReady} onPlay={playVideo} onPause={stopVideo} className='ratio ratio-16x9' />
+      <YouTube videoId={id} opts={opts} onReady={onPlayerReady}  onStateChange={handleStateChange} className='ratio ratio-16x9' />
     </div>
   )
 });
